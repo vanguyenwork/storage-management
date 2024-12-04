96d53e7f295a067cd9e3205b097935059a41b69c
Chloe <vanguyen.work@gmail.com>
2024-12-04 13:39:28 +0700

    fix build
    Signed-off-by: Chloe <vanguyen.work@gmail.com>

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,6 +5,8 @@ import { getCurrentUser } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
 import React, { ReactNode } from "react";
 
+export const dynamic = "force-dynamic";
+
 const Layout = async ({ children }: { children: ReactNode }) => {
   const currentUser = await getCurrentUser();
   if (!currentUser) {
