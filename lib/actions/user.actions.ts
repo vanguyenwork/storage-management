a7b6a2a5ece94ae066fa02268d8edf3fd2ae8d31
Chloe <vanguyen.work@gmail.com>
2024-12-04 13:53:09 +0700

    fix deployment

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -106,7 +106,8 @@ export const getCurrentUser = async () => {
 
     return parseStringify(user.documents[0]);
   } catch (error) {
-    handleError(error, "Failed to get current user");
+    console.log(error);
+    return null;
   }
 };
 
